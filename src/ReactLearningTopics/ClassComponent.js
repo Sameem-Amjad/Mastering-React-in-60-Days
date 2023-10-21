83d359f669e38a95a353d7ee41a86675942f6490
Sameem-Amjad <sameemamjadarsu@gmail.com>
2023-10-21 20:22:24 +0500

    React class component with a simple header, styled with CSS class.

---
+++ src/ReactLearningTopics/ClassComponent.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+export default class CLassComponent extends Component
+{ 
+    render ()
+    {
+        return (
+            <div className='classComponent'>
+                <h1>Class Components</h1>
+            </div>
+        );
+    }  
+}
